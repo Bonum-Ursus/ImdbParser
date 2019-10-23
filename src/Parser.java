1d2e381d3b611306543cb2f00cb9f88aa26bd344
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2019-10-23 21:52:44 +0500

    Изменил число потоков пулле с 2 на 6

--- src/Parser.java
+++ src/Parser.java
@@ -15,7 +15,7 @@ public class Parser {
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         DatabaseIMDb db = new DatabaseIMDb();
         db.createDBTables();
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ExecutorService executorService = Executors.newFixedThreadPool(6);
         for (int i = 1_000_000; i < 1_001_000; i++) {
             executorService.submit(new FilmThread(i));
         }
